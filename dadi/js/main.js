28284e5ac7b25093f82360e6420a90e0442c9f48
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-13 20:12:09 +0100

    spoiler

---
+++ dadi/js/main.js
@@ -0,0 +1,4 @@
+let randomUtente = Math.floor(Math.random() * 6 + 1);
+console.log(randomUtente);
+let randomCPU = Math.floor(Math.random() * 6 + 1);
+console.log(randomCPU);
--- mail/js/main.js
+++ mail/js/main.js
@@ -9,11 +9,13 @@ inviaElement.addEventListener('click', function (){
     for (let i = 0; i < lista.length; i++) {
 
         if (mail == lista[i]) {
-            controlloElement.innerText = ("Sei in lista, congratulazioni!");
+            // controlloElement.innerText = ("Sei in lista, congratulazioni!");
+            // dadiElement.innerHTML = (`<a href="../dadi/index.html">Entra pure</a>`);
+            document.location.href = '../dadi/index.html';
             break;
         }
          else{
-             controlloElement.innerText = ("Non sei in lista, mi dispiace!");
+            controlloElement.innerText = ("Ti devi ancora registrare");
          }
     }
 });
