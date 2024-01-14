dcb5a918388f4d15c9de459791f8ba54aaea3ca2
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-14 17:45:48 +0100

    dadi finale

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,4 +1,35 @@
+const resultCPU = document.getElementById('risultato-cpu');
+const resultUser = document.getElementById('risultato-utente');
+const resultTotal = document.getElementById('risultato-totale');
+const gioca = document.getElementById('bottone');
+// const annullo = document.getElementById('resetto');
+
+// Generare un numero da 1 a 6 per l'utente
 let randomUtente = Math.floor(Math.random() * 6 + 1);
-console.log(randomUtente);
+resultUser.innerText = `il numero dell'utente è ` + randomUtente;
+
+// Generare un numero da 1 a 6 per la cpu
 let randomCPU = Math.floor(Math.random() * 6 + 1);
-console.log(randomCPU);
+resultCPU.innerText = `il numero della cpu è ` + randomCPU;
+
+gioca.addEventListener('click', function(){
+
+    if(randomUtente > randomCPU){
+        resultTotal.innerText = `L'uomo batte la macchina`
+    }
+    else if(randomCPU > randomUtente){
+        resultTotal.innerText = `La macchina batte l'uomo`
+    }
+    else if(randomUtente == randomCPU || randomCPU == randomUtente){
+        resultTotal.innerText = `Purtroppo siamo pari`
+    }
+    // resultCPU.classList.remove('d-none');
+    // resultUser.classList.remove('d-none');
+    // resultTotal.classList.remove('d-none');
+})
+
+// annullo.addEventListener('click', function(){
+//     resultCPU.classList.add('d-none');
+//     resultUser.classList.add('d-none');
+//     resultTotal.classList.add('d-none');
+// })
--- mail/js/main.js
+++ mail/js/main.js
@@ -1,17 +1,27 @@
+// Dichiariamo le variabili/constanti
 const inviaElement = document.getElementById('invia');
 const emailElement = document.getElementById('email');
 const controlloElement = document.getElementById('controllo');
 const dadiElement = document.getElementById('dadi');
 
+// Creiamo una lista
 const lista = ['mariorossi@gmail.com', 'demetriopadre@gmail.com', 'pakysimeone@gmail.com', 'tizianonicolai@gmail.com'];
+
+// Aggiungiamo la funzione click al bottone
 inviaElement.addEventListener('click', function (){
     const mail = emailElement.value
+
+    // Creiamo un ciclo
     for (let i = 0; i < lista.length; i++) {
 
+        // controlliamo se la mail inserita è nella lista
         if (mail == lista[i]) {
-            // controlloElement.innerText = ("Sei in lista, congratulazioni!");
-            // dadiElement.innerHTML = (`<a href="../dadi/index.html">Entra pure</a>`);
-            document.location.href = '../dadi/index.html';
+
+            // Stampiamo "Sei in lista..." e il link per giocare ai dadi
+            controlloElement.innerText = ("Sei in lista, congratulazioni!");
+            dadiElement.innerHTML = (`<a href="../dadi/index.html">Entra pure</a>`);
+            // Altro metodo perandare nella pagina del gioco dei dadi direttamente cliccando sul bottone
+            // // document.location.href = '../dadi/index.html';
             break;
         }
          else{
